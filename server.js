aaa954676effbcb5e939383ff08b24c40d4a8050
adcoelho <antoniodcoelho@gmail.com>
2016-02-18 19:40:27 +0000

    Added the language parsing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var uaparser = require('ua-parser-js');
+var locale = require('locale');
 
 var server = http.createServer(function (req, res) {
     var result = {
@@ -11,8 +12,10 @@ var server = http.createServer(function (req, res) {
     result.software = `${ua.os.name} ${ua.os.version}`;
     var ip = req.connection.remoteAddress;
     result.ipaddress = ip.substring(ip.lastIndexOf(':') + 1); // Strip the ::ffff:
+    var locales = new locale.Locales(req.headers["accept-language"])
+    result.language = locales.best();
     res.end(JSON.stringify(result));
 });
 
-//server.listen(process.env.PORT || 5000);
-server.listen(8080);
+server.listen(process.env.PORT || 5000);
+//server.listen(8080);
